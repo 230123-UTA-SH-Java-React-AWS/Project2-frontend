8d0395d4546041812051ead8d996571927098984
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2023-03-21 10:59:41 -0700

    Allowed props import to change state of parent game component

--- src/components/GameScreen/LoadScreen.tsx
+++ src/components/GameScreen/LoadScreen.tsx
@@ -1,29 +1,37 @@
 import { BASE_URL, GAME_PORT } from "../../static/defaults";
 import axios, {AxiosRequestConfig} from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-function LoadScreen() {
-    const startGame = (gameid: string) => {
+function LoadScreen(props: any) {
+    // const [openGame, setOpenGame] = useState(false); 
+    
+   const startGame = () => {
         //axios request with gameID here
-
-        const requestConfig: AxiosRequestConfig = {
+        // Extract game ID from URL somehow
+        /*const requestConfig: AxiosRequestConfig = {
             baseURL: `http://${BASE_URL}:${GAME_PORT}`,
             headers: {
-                "gameId": gameid // <-- refactor later to extract this property from WHEREVER
+                "gameId": gameId, // <-- refactor later to extract this property from WHEREVER
+                "Content-Type": "application/json"
             }
         }
 
         const PATH = `/startBlackjackGame`; 
 
         axios.put<string>(PATH, {}, requestConfig).then((res) => {
+            setOpenGame(true);
             
-        })
-    }
+        }).catch((err) => console.log(err)); */
+        props.setOpenGameToTrue();
+    } 
     
-    return <div>
+    return (
+    <div>
         <h1>LOADING GAME...</h1>
         {/* Conditionally render this so only host player sees this */}
-        <button onClick{startGame}>Begin dealing</button>
-    </div>
+        <button type="button" onClick={startGame}>Begin dealing</button>
+    </div>)
 }
 
 export default LoadScreen;
