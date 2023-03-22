997a4abe73aacda8a156e1ad991fdd0d99cd75b2
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2023-03-22 13:18:29 -0700

    Added a function to check if a player is host DOESN'T MATTER SINCE COMPONENT MAY BE TRASHED ANYWAY

--- src/components/LoadScreen/LoadScreen.tsx
+++ src/components/LoadScreen/LoadScreen.tsx
@@ -3,40 +3,48 @@ import axios, { AxiosRequestConfig } from "axios";
 import "./LoadScreen.css";
 import { useNavigate } from "react-router-dom";
 
-function LoadScreen(props: any) {
+function LoadScreen() {
     const navigate = useNavigate();
-    const startGame = () => {
-        // Extract game ID from URL and use that to send in request
-        const gameId = window.location.pathname.split("/")[2];
-        console.log(gameId);
-        const requestConfig: AxiosRequestConfig = {
-            baseURL: `http://${BASE_URL}:${GAME_PORT}`,
-            headers: {
-                "gameId": gameId, 
-                "playerId": "", // I don't know how to retrieve this piece of data
-                "Content-Type": "application/json"
-            }
+
+    const gameId = window.location.pathname.split("/")[2];
+    console.log(gameId);
+    const requestConfig: AxiosRequestConfig = {
+        baseURL: `http://${BASE_URL}:${GAME_PORT}`,
+        headers: {
+            "gameId": gameId,
+            "playerId": "", // I don't know how to retrieve this piece of data
+            "Content-Type": "application/json"
         }
+    }
 
-        const PATH = `/startBlackjackGame`; 
-
+    function checkIfPlayerIsHost() {
+        const HOSTPATH = `/amIHost`;
+        axios.get<string>(HOSTPATH, requestConfig).then((res) => {
+            return res.data;
+        })
+    }
+    
+    function startGame() {
+        // Extract game ID from URL and use that to send in request
+        const PATH = `/startBlackjackGame`;
         axios.put<string>(PATH, {}, requestConfig).then((res) => {
             // props.setOpenGameToTrue();
-            navigate("/" + "blackjack" + "/" + gameId); 
-            
+            navigate("/" + "blackjack" + "/" + gameId);
+
         }).catch((err) => console.log(err));
-        
-        // This is a test method when backend is not connected - can be deleted later 
-        // props.setOpenGameToTrue();
+
+
     }
 
     return (
         <div className="loadscreen">
             <h1 className="load-header">LOADING GAME...</h1>
+            <button>JOIN</button>
             {/* Conditionally render this so only host player sees this... somehow */}
-            
-                <button className="load-start-btn" type="button" onClick={startGame}>Begin dealing</button>
-            
+            <button className="load-start-btn" type="button" onClick={startGame}>Begin dealing</button>
+
+
+
         </div>)
 }
 
