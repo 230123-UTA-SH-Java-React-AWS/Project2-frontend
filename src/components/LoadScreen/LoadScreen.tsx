e42dc13a9ca67427794bd548126b69eab70b597e
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2023-03-21 17:33:26 -0700

    Changed from nested component to separate component

--- src/components/LoadScreen/LoadScreen.tsx
+++ src/components/LoadScreen/LoadScreen.tsx
@@ -1,8 +1,10 @@
 import { BASE_URL, GAME_PORT } from "../../static/defaults";
 import axios, { AxiosRequestConfig } from "axios";
 import "./LoadScreen.css";
+import { useNavigate } from "react-router-dom";
 
 function LoadScreen(props: any) {
+    const navigate = useNavigate();
     const startGame = () => {
         // Extract game ID from URL and use that to send in request
         const gameId = window.location.pathname.split("/")[2];
@@ -19,18 +21,19 @@ function LoadScreen(props: any) {
         const PATH = `/startBlackjackGame`; 
 
         axios.put<string>(PATH, {}, requestConfig).then((res) => {
-            props.setOpenGameToTrue();
+            // props.setOpenGameToTrue();
+            navigate("/" + "blackjack" + "/" + gameId); 
             
         }).catch((err) => console.log(err));
         
         // This is a test method when backend is not connected - can be deleted later 
-        props.setOpenGameToTrue();
+        // props.setOpenGameToTrue();
     }
 
     return (
         <div className="loadscreen">
             <h1 className="load-header">LOADING GAME...</h1>
-            {/* Conditionally render this so only host player sees this */}
+            {/* Conditionally render this so only host player sees this... somehow */}
             
                 <button className="load-start-btn" type="button" onClick={startGame}>Begin dealing</button>
             
