86ab7b69fc9eb561b7e06166425f7043c3340c67
connor-eg <connoregarcia@gmail.com>
2023-03-28 13:54:06 -0500

    urls fixed

--- src/util/lobbyClient.ts
+++ src/util/lobbyClient.ts
@@ -1,14 +1,16 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 import { store } from "../redux/store";
+import { BASE_URL, GAME_PORT } from "../static/defaults";
 
 const lobbyClient = axios.create({
-  baseURL: "http://localhost:4798/",
+  baseURL: `http://${BASE_URL}:${GAME_PORT}/`,
   withCredentials: true, // This is important for sending and receiving cookies
 });
 
 lobbyClient.interceptors.request.use(
   (config) => {
-    const csrfToken = store.getState().csrf.token;
+    const csrfToken = Cookies.get("XSRF-TOKEN");
     if (csrfToken) {
       config.headers["X-XSRF-TOKEN"] = csrfToken;
     }
