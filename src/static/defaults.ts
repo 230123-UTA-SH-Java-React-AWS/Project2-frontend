55aeff935ffa6373ede753ecd6241901880896a1
Jahi-Citizen <jahi731@revature.net>
2023-03-29 15:41:05 -0400

    merge resolutions

--- src/static/defaults.ts
+++ src/static/defaults.ts
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
-export const BASE_URL: string = "localhost"
-=======
 export const BASE_URL: string = "stephens-blackjack.eastus.cloudapp.azure.com"
->>>>>>> 0ba7922a6922665aacaf12b0b8a82f35dbf69240
 export const GAME_PORT: string = "4798"
